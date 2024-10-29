06f92de84d6677e383a759c827d600333124f973
ShoneStew <macif50508@gmail.com>
2024-10-29 21:17:40 +0300

    Мелкие исправления + документация к коду.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Импорт файла ниже нужен чтобы поднимать бота, и обходить ограничения хостингов по типу Render по времени бездействия.
 const keep_alive = require('./keep_alive.js');
+// Импорт нужных библиотек и фреймворков для работы бота.
 const { Telegraf, Markup } = require('telegraf');
 const axios = require('axios');
 const MongoClient = require("mongodb").MongoClient;
 require('dotenv').config();
+// В файле ".env" надо вписать следующие параметры:
+// TOKEN - токен бота,
+// URL - ссылку на базу данных, типо такого: "mongodb://username:password@example.com/dbname",
+// DB - название базы данных.
 
+// Проверка на администратора, путём сравнения айди отправителя команды с айди администраторов чата/группы.
 async function isAdmin(ctx) {
     const chatId = ctx.message.chat.id;
     const userId = ctx.message.from.id;
@@ -17,6 +23,7 @@ async function isAdmin(ctx) {
     };
 };
 
+// Проверка статуса сервера по API mcsrvstat. Проверяется сразу на 2 изданиях, если не получилось вычислить состояние сервера по обеим изданиям - выдается false.
 async function serverStatus(ip, port) {
     try {
         const res_java = await (await axios.get(`http://api.mcsrvstat.us/3/${ip}:${port}`)).data;
@@ -33,75 +40,102 @@ async function serverStatus(ip, port) {
     };
 };
 
+// Вход в бота и в базу данных.
 const bot = new Telegraf(process.env.TOKEN);
-
 const mdb = new MongoClient(process.env.URL);
 
+// Добавление сервера.
 bot.command('addserver', async (ctx) => {
     try {
+        // Входим в базу данных.
         await mdb.connect();
+        // Объявляем нужные переменные.
         const chatId = ctx.message.chat.id.toString();
         const args = ctx.message.text.slice(1).split(' ');
         const ip = args[1];
         const port = args[2];
         const slot = parseInt(args[3], 10);
         const adminCheck = await isAdmin(ctx);
+        // Обращаемся в базе данных по коллекции. Если не будет коллекции с соответствующим именем - создается новая.
         const coll = mdb.db(process.env.DB).collection(`servers${slot}`);
         const findColl = await coll.find({ chatId }).toArray();
 
+        // Проверка на тип чата.
         if (ctx.message.chat.type === 'private') return;
 
+        // Проверка на администратора.
         if (!adminCheck) return;
 
+        // Если введённый слот больше 3 или меньше 1 - то это неверно указанный слот.
         if (slot < 1 || slot > 3) {
             await ctx.telegram.sendMessage(chatId, '❌ Вы указали неверный слот!');
+        // Если забыли ввести айпи-адрес и порт, или слот не является числом.
         } else if (!ip || !port || isNaN(slot)) {
             await ctx.telegram.sendMessage(chatId, '❌ Вы не указали один из параметров!');
-        } else if (findColl.length >= 1) {
+        // Если уже есть информация об сервере в введённом слоту.
+        } else if (findColl.length > 0) {
             await ctx.telegram.sendMessage(chatId, '❌ В данном слоте уже записана информация об сервере!');
+        // Если все условия миновали - то добавляется сервер в коллекцию.
         } else {
+            // Добавляем в коллекцию JSON-файл.
             await coll.insertOne({chatId, ip, port});
+            // Отправляем соообщение об успешном добавлении сервера в коллекцию.
             ctx.telegram.sendMessage(chatId, '💾 Информация об сервере сохранена!');
         };
     } catch (e) {
+        // Ловим ошибки.
         console.log(e);
     } finally {
+        // Закрываем подключение к базе данных.
         await mdb.close();
     };
 });
 
+// Проверка статуса по слотам.
 bot.command('status', async (ctx) => {
     try {
+        // Входим в базу данных.
         await mdb.connect();
+        // Объявляем нужные переменные.
         const chatId = ctx.message.chat.id.toString();
         const args = ctx.message.text.slice(1).split(' ');
         const slot = parseInt(args[1], 10);
+        // Обращаемся в базе данных по коллекции. Если не будет коллекции с соответствующим именем - создается новая.
         const coll = mdb.db(process.env.DB).collection(`servers${slot}`);
         const findColl = await coll.find({ chatId }).toArray();
 
+        // Проверка на тип чата.
         if (ctx.message.chat.type === 'private') return;
 
-        if (slot > -1 || slot < 3) {
-            const ip = findColl[0]?.ip;
-            const port = findColl[0]?.port;
+        // Если введённый слот не больше 3 или больше 1 - то это верно указанный слот.
+        if (slot && slot > 1 || slot <= 3) {
+            // Объявляем нужные переменные.
+            const ip = findColl[0].ip;
+            const port = findColl[0].port;
+            // Получаем статус сервера в JSON-строке.
             const res = await serverStatus(ip, port);
             let stat;
 
+            // Если в слоте нету сервера.
             if (findColl.length < 0) {
                 await ctx.telegram.sendMessage(chatId, `😔 В слоте №${slot} нету добавленного сервера.`);
                 return;
             };
 
+            // Если сервер отключён.
             if (res.online === false) {
                 stat = `отключён.\n📘 Айпи-адрес и порт: <code>${ip}</code>/<code>${port}</code>.`
+            // Если сервер включён.
             } else {
                 stat = `включён!\n📘 Айпи-адрес и порт: <code>${ip}</code>/<code>${port}</code>,\n👥 Игроков онлайн: ${res.players.online}/${res.players.max},\n📙 Версия: ${res.version} \n📃 Описание сервера: ${res.motd.clean}`
             };
 
+            // Отправляем соообщение об статусе сервера в указанном слоту.
             await ctx.telegram.sendMessage(chatId, `🔌 Состояние сервера в слоте №${slot} - ${stat}`, {
                 parse_mode: 'HTML'
             });
         } else if (!slot) {
+            // Отрпавляем сообщение с инлайн-кнопками слотов.
             await ctx.telegram.sendMessage(chatId, '🗂 Выберите нужный слот ниже дабы посмотреть его статус:', Markup.inlineKeyboard([
                 [Markup.button.callback('📂 Слот №1', 'serv1')],
                 [Markup.button.callback('📂 Слот №2', 'serv2')],
@@ -109,42 +143,59 @@ bot.command('status', async (ctx) => {
             ]));
         };
     } catch (e) {
+        // Ловим ошибки.
         console.log(e);
     } finally {
+        // Закрываем подключение к базе данных.
         await mdb.close();
     };
 });
 
+// Удаление сервера с коллекцию.
 bot.command("deleteserver", async (ctx) => {
     try {
+        // Входим в базу данных.
         await mdb.connect();
+        // Объявляем нужные переменные.
         const chatId = ctx.message.chat.id.toString();
         const args = ctx.message.text.slice(1).split(' ');
         const slot = parseInt(args[1]);
         const adminCheck = await isAdmin(ctx);
+        // Обращаемся в базе данных по коллекции. Если не будет коллекции с соответствующим именем - создается новая.
         const coll = mdb.db(process.env.DB).collection(`servers${slot}`);
         const findColl = await coll.find({ chatId }).toArray();
 
+        // Проверка на тип чата.
         if (ctx.message.chat.type === 'private') return;
 
+        // Проверка на администратора.
         if (!adminCheck) return;
 
-        if (isNaN(slot) || slot < 1 || slot > 3) {
+        // Если введённый слот больше 3 или меньше 1 - то это неверно указанный слот.
+        if (slot < 1 || slot > 3) {
             await ctx.telegram.sendMessage(chatId, '❌ Вы указали неверный слот!');
+        // Если забыли ввести айпи-адрес и порт, или слот не является числом.
         } else if (findColl.length > 1) {
             await ctx.telegram.sendMessage(chatId, '❌ В данном слоте нету информации об сервере!');
+        // Если все условия миновали - то удаляем сервер из коллекции.
         } else {
+            // Удаляем сервер из коллекции по айди чата.
             await coll.deleteMany({ chatId });
+            // Отправляем соообщение об успешном удалении сервера из коллекции.
             await ctx.telegram.sendMessage(chatId, '🗑 Информация об сервере удалена!');
         };
     } catch (e) {
+        // Ловим ошибки.
         console.log(e);
     } finally {
+        // Закрываем подключение к базе данных.
         await mdb.close();
     };
 });
 
+// Помощь по командам.
 bot.command("help", async (ctx) => {
+    // Отправляем сообщение с инлайн-кнопками, чтобы по ним переключаться по разделу помощи.
     await ctx.telegram.sendMessage(ctx.message.chat.id, '📄 Выберите нужную команду дабы посмотреть инструкцию на него:', Markup.inlineKeyboard([
         [Markup.button.callback('🔌 /status', 'status')],
         [Markup.button.callback('💾 /addserver', 'addserver')],
@@ -154,83 +205,47 @@ bot.command("help", async (ctx) => {
 
 bot.on("callback_query", async (ctx) => {
     try {
+        // Входим в базу данных.
         await mdb.connect();
+        // Объявляем нужные переменные.
         const chatId = ctx.update.callback_query.message.chat.id.toString();
         const callId = ctx.update.callback_query.data;
 
         // Ниже реагирование на слоты.
-        if (callId === 'serv1') {
-            const coll = mdb.db(process.env.DB).collection('servers1');
+        if (callId.startsWith('serv')) {
+            // Объявляем нужные переменные.
+            const slot = callId.slice(4);
+            const coll = mdb.db(process.env.DB).collection(`servers${slot}`);
             const findColl = await coll.find({ chatId }).toArray();
 
+            // Если в слоте нету сервера.
             if (findColl.length < 1) {
-                await ctx.editMessageText('😔 В слоте №1 нету добавленного сервера.');
+                await ctx.editMessageText(`😔 В слоте №${slot} нету добавленного сервера.`);
                 return;
             };
 
+            // Продолжается обьявление нужных переменных.
             const ip = findColl[0].ip;
             const port = findColl[0].port;
+            // Получаем статус сервера в JSON-строке.
             const res = await serverStatus(ip, port);
             let stat;
 
+            // Если сервер отключён.
             if (res.online === false) {
                 stat = `отключён.\n📘 Айпи-адрес и порт: <code>${ip}</code>/<code>${port}</code>.`
+            // Если сервер включён.
             } else {
                 stat = `включён!\n📘 Айпи-адрес и порт: <code>${ip}</code>/<code>${port}</code>,\n👥 Игроков онлайн: ${res.players.online}/${res.players.max},\n📙 Версия: ${res.version} \n📃 Описание сервера: ${res.motd.clean}`
             };
 
-            await ctx.editMessageText(`🔌 Состояние сервера в слоте №1 - ${stat}`, {
-                parse_mode: 'HTML'
-            });
-        } else if (callId === 'serv2') {
-            const coll = mdb.db(process.env.DB).collection('servers2');
-            const findColl = await coll.find({ chatId }).toArray();
-
-            if (findColl.length < 1) {
-                await ctx.editMessageText('😔 В слоте №2 нету добавленного сервера.');
-                return;
-            };
-
-            const ip = findColl[0].ip;
-            const port = findColl[0].port;
-            const res = await serverStatus(ip, port);
-            let stat;
-
-            if (res.online === false) {
-                stat = `отключён.\n📘 Айпи-адрес и порт: <code>${ip}</code>/<code>${port}</code>.`
-            } else {
-                stat = `включён!\n📘 Айпи-адрес и порт: <code>${ip}</code>/<code>${port}</code>,\n👥 Игроков онлайн: ${res.players.online}/${res.players.max},\n📙 Версия: ${res.version} \n📃 Описание сервера: ${res.motd.clean}`
-            };
-
-            await ctx.editMessageText(`🔌 Состояние сервера в слоте №2 - ${stat}`, {
-                parse_mode: 'HTML'
-            });
-        } else if (callId === 'serv3') {
-            const coll = mdb.db(process.env.DB).collection('servers3');
-            const findColl = await coll.find({ chatId }).toArray();
-
-            if (findColl.length < 1) {
-                await ctx.editMessageText('😔 В слоте №3 нету добавленного сервера.');
-                return;
-            };
-
-            const ip = findColl[0].ip;
-            const port = findColl[0].port;
-            const res = await serverStatus(ip, port);
-            let stat;
-
-            if (res.online === false) {
-                stat = `отключён.\n📘 Айпи-адрес и порт: <code>${ip}</code>/<code>${port}</code>.`
-            } else {
-                stat = `включён!\n📘 Айпи-адрес и порт: <code>${ip}</code>/<code>${port}</code>,\n👥 Игроков онлайн: ${res.players.online}/${res.players.max},\n📙 Версия: ${res.version} \n📃 Описание сервера: ${res.motd.clean}`
-            };
-
-            await ctx.editMessageText(`🔌 Состояние сервера в слоте №3 - ${stat}`, {
+            // Отправляем соообщение об статусе сервера в указанном слоту.
+            await ctx.editMessageText(`🔌 Состояние сервера в слоте №${slot} - ${stat}`, {
                 parse_mode: 'HTML'
             });
         // Ниже туторы на команды.
         } else if (callId === 'status') {
-            await ctx.editMessageText('Команда \/status выводит список слотов, в которых хранятся данные об сервере.\nАргументом можно вставить слот от 1 до 3, например: \"/status 2\".', Markup.inlineKeyboard([
+            await ctx.editMessageText('Команда /status выводит список слотов, в которых хранятся данные об сервере.\nАргументом можно вставить слот от 1 до 3, например: \"/status 2\".', Markup.inlineKeyboard([
                 [Markup.button.callback('💾 /addserver', 'addserver')],
                 [Markup.button.callback('🗑 /deleteserver', 'deleteserver')],
                 [Markup.button.callback('🔙 Назад', 'back')],
@@ -255,39 +270,50 @@ bot.on("callback_query", async (ctx) => {
             ]));
         }
     } catch (e) {
+        // Ловим ошибки.
         console.log(e);
     } finally {
+        // Закрываем подключение к базе данных.
         await mdb.close();
     };
 });
 
 bot.on('inline_query', async (ctx) => {
     try {
+        // Объявляем нужные переменные.
         const text = ctx.inlineQuery.query.trim().split(' ');
         const results = [];
 
+        // Проверяем, введён ли текст в инлайн-режиме.
         if (!text[0] || !text[1]) {
+            // Отправляем в массив объект, который отобразиться в инлайн-режиме.
             results.push({
                 type: 'article',
                 id: 'without_text',
                 title: 'Проверка статуса',
-                description: "Пожалуйста, введите айпи адрес и порт в формате: \"@username example.com 19132\".",
+                description: "Пожалуйста, введите айпи адрес и порт в формате: \"@man_checkStatus_bot play.hypixel.net 25565\".",
                 input_message_content: {
-                    message_text: '❌ Введите айпи адрес и порт в формате: \"@username example.com 19132\".',
+                    message_text: '❌ Введите айпи адрес и порт в формате: \"<code>@man_checkStatus_bot play.hypixel.net 25565</code>\".',
+                    parse_mode: 'HTML',
                 },
             });
         } else {
+            // Объявляем нужные переменные.
             const ip = text[0];
             const port = text[1];
+            // Получаем статус сервера в JSON-строке.
             const res = await serverStatus(ip, port);
             let stat;
 
+            // Если сервер отключён.
             if (res.online === true) {
                 stat = `включён!\n📘 Айпи-адрес и порт: <code>${ip}</code>/<code>${port}</code>,\n👥 Игроков онлайн: ${res.players.online}/${res.players.max},\n📙 Версия: ${res.version} \n📃 Описание сервера: ${res.motd.clean}`;
+            // Если сервер включён.
             } else {
                 stat = `отключён.\n📘 Айпи-адрес и порт: <code>${ip}</code>/<code>${port}</code>.`;
             };
 
+            // Отправляем в массив объект, который отобразиться в инлайн-режиме и в котором содержится информация об статусе сервера.
             results.push({
                 type: 'article',
                 id: 'with_text',
@@ -300,10 +326,13 @@ bot.on('inline_query', async (ctx) => {
             });
         };
 
+        // Отправляем массив с объектами в инлайн-режим.
         await ctx.answerInlineQuery(results);
     } catch (e) {
+        // Ловим ошибки.
         console.log(e);
     };
 });
 
+// Запускаем бота.
 bot.launch();
